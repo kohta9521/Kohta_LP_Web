bda1598331c5c64569eb73a2d6c3f02647740e95
k_95.21 <kohta0421kochi@gmail.com>
2025-01-10 16:18:14 +0900

    delete all file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,12 @@
 // scss
-import HeroContent from "@/components/molecules/HeroContent";
 import styles from "../styles/page.module.scss";
 
 // components
-import Header from "@/components/organism/Header";
-import Hero from "@/components/organism/Hero";
-import Worries from "@/components/organism/Worries";
-import Realize from "@/components/organism/Realize";
-import ContactBanner from "@/components/organism/ContactBanner";
-import Achievement from "@/components/organism/Achievement";
-import Seo from "@/components/organism/Seo";
-import Review from "@/components/organism/Review";
 
 export default function Home() {
   return (
     <div>
-      <Header />
-      <Hero>
-        <HeroContent />
-      </Hero>
-      <Worries />
-      <Realize />
-      <ContactBanner />
-      <Achievement />
-      <Seo />
-      <Review />
+      <h1>hello world</h1>
     </div>
   );
 }
